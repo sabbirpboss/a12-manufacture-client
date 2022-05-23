fff26bc6be3004b08dc98c26de8b3ece52fe4dc9
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-23 22:15:39 +0600

    initial test commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-2xl text-red-500 font-bold'>Industo</h1>
+      <button class="btn btn-primary">Info</button>
     </div>
   );
 }
